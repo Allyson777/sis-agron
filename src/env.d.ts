eac211947191a74807dfd78fb1081e708bdaab5d
Allyson Pina <allyson.viana@edu.ufes.br>
2025-02-11 23:15:50 -0300

    create environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,13 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NODE_ENV: string;
-    VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
-    VUE_ROUTER_BASE: string | undefined;
+    NODE_ENV: string
+    VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined
+    VUE_ROUTER_BASE: string | undefined
+    VUE_APP_FIREBASE_API_KEY: string
+    VUE_APP_FIREBASE_AUTH_DOMAIN: string
+    VUE_APP_FIREBASE_PROJECT_ID: string
+    VUE_APP_FIREBASE_STORAGE_BUCKET: string
+    VUE_APP_FIREBASE_MESSAGING_SENDER_ID: string
+    VUE_APP_FIREBASE_APP_ID: string
   }
 }
--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA4ZG36QtPIrkz8gZMz-iRekxGoEcQBQmE',
-  authDomain: 'sis-agron1.firebaseapp.com',
-  projectId: 'sis-agron1',
-  storageBucket: 'sis-agron1.firebasestorage.app',
-  messagingSenderId: '841569083698',
-  appId: '1:841569083698:web:3a776c0f2e731d75c5772b',
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
 }
 
 // Inicializa o Firebase
